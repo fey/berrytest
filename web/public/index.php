2074506625d10c2b242908e5789c43fc1f10f9de
fey <feycot@gmail.com>
2018-09-23 11:37:28 +0500

    add support for built-in php server

--- web/public/index.php
+++ web/public/index.php
@@ -3,6 +3,16 @@
 namespace App;
 
 error_reporting(E_ALL & ~E_NOTICE);
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__.$url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+var_dump($_SERVER);
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new App();
