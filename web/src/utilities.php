c76c61701bca8c1dde4a50badd5dd3e1d9619e3b
fey <feycot@gmail.com>
2018-10-14 15:54:54 +0500

    add utulities.php

---
+++ web/src/utilities.php
@@ -0,0 +1,11 @@
+<?php
+
+function clean($value = '')
+{
+    $value = trim($value);
+    $value = stripslashes($value);
+    $value = strip_tags($value);
+    $value = htmlspecialchars($value);
+
+    return $value;
+}
