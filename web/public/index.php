ae5c13f684a7f5c68d9d66e6912eff9b3c2ce4d8
fey <feycot@gmail.com>
2018-10-18 12:18:19 +0500

    add routes

--- web/public/index.php
+++ web/public/index.php
@@ -4,15 +4,49 @@ namespace App;
 
 require_once '../vendor/autoload.php';
 use function App\Renderer\render;
+use Db\Repository;
 
+$repo = new Repository('articles');
 $app = new Application();
+// $meta, $params, $attributes, $cookies, $session
 
-$app->get('', function ($meta, $params, $attributes, $cookies, $session) {
-    $session->start();
-    $nickname = $session->get('nickname');
+$app->get('/', function ($meta, $params, $attributes, $cookies, $session) use ($repo) {
+    $articles = $repo->all();
 
-    return response(render('index', ['nickname' => $nickname]));
+    return response(render('index', ['articles' => $articles]));
 });
-$app->get('/test', '\App\Controller\Test::index');
-$app->get('/test/:id', '\App\Controller\Test::getUser');
+$app->get('/new', function ($meta, $params, $attributes, $cookies, $session) {
+    return response(render('articles/new'));
+});
+$app->get('/article/:id', function ($meta, $params, $attributes, $cookies, $session) use ($repo) {
+    $article = $repo->findBy('id', $attributes['id']);
+    if ($article) {
+        return response(var_dump($article));
+
+        return response(render('articles/index', ['article' => $article]));
+    }
+});
+
+$app->post('/articles', function ($meta, $params, $attributes, $cookies, $session) use ($repo) {
+    $formData = array_map('\clean', $params['article']);
+    // $articles->insert
+
+    $repo->insert([
+        'description' => $formData['description'],
+        'text' => str_replace(PHP_EOL, '</br>', $formData['text']),
+        'author_id' => 1,
+    ]);
+
+    return response()->redirect('/');
+});
+
+$app->delete('/articles', function ($meta, $params, $attributes, $cookies, $session) use ($repo) {
+    $truncate = $params['truncate'] ?? false;
+    if ($truncate) {
+        $repo->truncate('articles');
+    }
+
+    return response()->redirect('/');
+});
+
 $app->run();
