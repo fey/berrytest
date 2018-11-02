2a18eed76f949c6fbf65c203f534a9c384cbfe86
fey <feycot@gmail.com>
2018-11-03 00:59:41 +0500

    fix gitallheaders

--- src/App/Request.php
+++ src/App/Request.php
@@ -57,19 +57,16 @@ class Request
     public function getAllHeaders()
     {
         if (!function_exists('getallheaders')) {
-            function getallheaders()
-            {
-                $headers = [];
-                foreach ($_SERVER as $name => $value) {
-                    if (substr($name, 0, 5) == 'HTTP_') {
-                        $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-                    }
+            $headers = $this->getHeaders();
+            foreach ($_SERVER as $name => $value) {
+                if (substr($name, 0, 5) == 'HTTP_') {
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
                 }
-
-                return $headers;
             }
-        }
 
-        return getallheaders();
+            return $headers;
+        } else {
+            return \getallheaders();
+        }
     }
 }
