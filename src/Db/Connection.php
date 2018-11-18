13f0c58b24148b132a084865bf2eb62fafa455eb
fey <feycot@gmail.com>
2018-11-18 15:11:03 +0500

    db connecting via getenv

--- src/Db/Connection.php
+++ src/Db/Connection.php
@@ -10,20 +10,32 @@ class Connection
 
     public function __construct()
     {
-        $this->pdo = new PDO('pgsql:host=tantor.db.elephantsql.com;dbname=fihqhtuc;', 'fihqhtuc', 'aGGcjL1UX_h26cyWrIdwWtFdnYijcH44', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_PERSISTENT => true,
-        ]);
+        $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_PERSISTENT => true, ];
+        $db = parse_url(getenv('DATABASE_URL') ?: 'postgres://fihqhtuc:aGGcjL1UX_h26cyWrIdwWtFdnYijcH44@tantor.db.elephantsql.com:5432/fihqhtuc');
+        if ($db) {
+            $this->pdo = new PDO('pgsql:'.sprintf(
+                'host=%s;port=%s;user=%s;password=%s;dbname=%s',
+                $db['host'],
+                $db['port'],
+                $db['user'],
+                $db['pass'],
+                ltrim($db['path'], '/')
+            ), $options);
+        } else {
+            // $this->pdo = new PDO(
+            //     'pgsql:host=tantor.db.elephantsql.com;dbname=fihqhtuc;',
+            //     'fihqhtuc',
+            //     'aGGcjL1UX_h26cyWrIdwWtFdnYijcH44',
+            //     $options
+            // );
+        }
     }
 
     public function get()
     {
         return $this->pdo;
-    }
-
-    public function getConfig()
-    {
-        return parse_ini_file(__DIR__.DIRECTORY_SEPARATOR.'../config.ini');
     }
 }
