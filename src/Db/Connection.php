0cee2c4c01dfe757b46775be823d3d462d6b6efa
fey <feycot@gmail.com>
2018-11-18 16:09:48 +0500

    clean connect credentials

--- src/Db/Connection.php
+++ src/Db/Connection.php
@@ -10,12 +10,7 @@ class Connection
 
     public function __construct()
     {
-        $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_PERSISTENT => true, ];
-        $db = parse_url(getenv('DATABASE_URL') ?:
-        'postgres://fihqhtuc:aGGcjL1UX_h26cyWrIdwWtFdnYijcH44@tantor.db.elephantsql.com:5432/fihqhtuc');
+        $db = parse_url(getenv('DATABASE_URL'));
         $this->pdo = new PDO('pgsql:'.sprintf(
             'host=%s;port=%s;user=%s;password=%s;dbname=%s',
             $db['host'],
@@ -24,12 +19,9 @@ class Connection
             $db['pass'],
             ltrim($db['path'], '/')
         ));
-        // $this->pdo = new PDO(
-            //     'pgsql:host=tantor.db.elephantsql.com;dbname=fihqhtuc;',
-            //     'fihqhtuc',
-            //     'aGGcjL1UX_h26cyWrIdwWtFdnYijcH44',
-            //     $options
-            // );
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->pdo->setAttribute(PDO::ATTR_PERSISTENT, PDO::true);
     }
 
     public function get()
