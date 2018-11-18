34c7f6cd762d362b7490eae16a1bc13d7f68ba02
fey <feycot@gmail.com>
2018-11-18 20:22:41 +0500

    add db scheme

---
+++ db/berry.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS "articles";
+DROP SEQUENCE IF EXISTS articles_id_seq;
+CREATE SEQUENCE articles_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+
+CREATE TABLE "public"."articles" (
+    "id" integer DEFAULT nextval('articles_id_seq') NOT NULL,
+    "description" character varying(255) NOT NULL,
+    "text" text NOT NULL,
+    "author_id" integer NOT NULL,
+    "created_at" timestamp DEFAULT now(),
+    "title" character varying(255),
+    CONSTRAINT "articles_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "authors";
+DROP SEQUENCE IF EXISTS authors_id_seq;
+CREATE SEQUENCE authors_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+
+CREATE TABLE "public"."authors" (
+    "id" integer DEFAULT nextval('authors_id_seq') NOT NULL,
+    "name" character varying NOT NULL,
+    "ssid" character varying(255) NOT NULL,
+    CONSTRAINT "authors_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+
+DROP TABLE IF EXISTS "comments";
+DROP SEQUENCE IF EXISTS comments_id_seq;
+CREATE SEQUENCE comments_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1;
+
+CREATE TABLE "public"."comments" (
+    "id" integer DEFAULT nextval('comments_id_seq') NOT NULL,
+    "author_id" integer NOT NULL,
+    "article_id" integer NOT NULL,
+    "parent_id" integer DEFAULT '0',
+    "body" character varying(400),
+    "created_at" timestamp DEFAULT now(),
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+) WITH (oids = false);
