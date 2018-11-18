33172795525ee2ef3cc1f0ada8e436014c32ce02
fey <feycot@gmail.com>
2018-11-18 19:13:10 +0500

    move db connection to repository

--- src/Db/Connection.php
+++ src/Db/Connection.php
@@ -2,28 +2,8 @@
 
 namespace  Db;
 
-use PDO;
-
 class Connection
 {
-    private $pdo;
-
-    public function __construct()
-    {
-        $db = parse_url(getenv('DATABASE_URL'));
-        $this->pdo = new PDO('pgsql:'.sprintf(
-            'host=%s;port=%s;user=%s;password=%s;dbname=%s',
-            $db['host'],
-            $db['port'],
-            $db['user'],
-            $db['pass'],
-            ltrim($db['path'], '/')
-        ));
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $this->pdo->setAttribute(PDO::ATTR_PERSISTENT, true);
-    }
-
     public function get()
     {
         return $this->pdo;
--- src/Db/Repository.php
+++ src/Db/Repository.php
@@ -2,14 +2,27 @@
 
 namespace Db;
 
+use PDO;
+
 class Repository
 {
-    protected $pdo;
+    private $pdo;
     private $table;
 
     public function __construct($table)
     {
-        $this->pdo = (new Connection())->get();
+        $db = parse_url(getenv('DATABASE_URL'));
+        $this->pdo = new PDO('pgsql:'.sprintf(
+            'host=%s;port=%s;user=%s;password=%s;dbname=%s',
+            $db['host'],
+            $db['port'],
+            $db['user'],
+            $db['pass'],
+            ltrim($db['path'], '/')
+        ));
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->pdo->setAttribute(PDO::ATTR_PERSISTENT, true);
 
         $this->table = $table;
     }
