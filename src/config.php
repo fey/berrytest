fa3202cf285e722b576c80893f70c63c44a06c0e
fey <feycot@gmail.com>
2019-04-20 17:01:38 +0500

    fix config

--- src/config.php
+++ src/config.php
@@ -2,6 +2,7 @@
 use function Utilities\getRootDir;
 
 const CONFIG_PATH = __DIR__ . '/../config.ini';
-if (file_exists(CONFIG_PATH)) {
+
+if (file_exists(CONFIG_PATH) || (getenv('DATABASE_URL') === null)) {
     putenv("DATABASE_URL=" . parse_ini_file(CONFIG_PATH)['DATABASE_URL']);
 }
