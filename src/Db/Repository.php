dd3ad081c49f23c04219ab76218ffc5df960bdd6
fey <feycot@gmail.com>
2019-04-20 17:55:32 +0500

    clear code

--- src/Db/Repository.php
+++ src/Db/Repository.php
@@ -20,6 +20,7 @@ class Repository
             $db['pass'],
             ltrim($db['path'], '/')
         ));
+
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $this->pdo->setAttribute(PDO::ATTR_PERSISTENT, true);
